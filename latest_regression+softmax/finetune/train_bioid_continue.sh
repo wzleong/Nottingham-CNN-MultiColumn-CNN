8fbab30ede55420d8e6c66acb1d99e3f646e13de
wzleong <lwzhen1119@gmail.com>
2016-10-30 00:35:35 +0100

    latest regression and softmax CNN ~ working

---
+++ latest_regression+softmax/finetune/train_bioid_continue.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+# examples/BIOID_face/train_bioid.sh examples/BIOID_face/
+
+
+./build/tools/caffe train -solver examples/nottingham_multi_label/bioid_solver.prototxt -weights $1 2>&1 | tee $2
