e93825b70a05063ddab893446a05e4e276f919cf
wzleong <lwzhen1119@gmail.com>
2016-11-12 18:54:35 +0000

    remove all unneccesary files

--- nottngham_with_scaling_tranlation_eye_localization/train_bioid_complete.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-# examples/BIOID_face/train_bioid.sh examples/BIOID_face/
-
-./build/tools/caffe train --solver=examples/nottingham_new/bioid_solver_finetune.prototxt -weights $1 2>&1 | tee $2
